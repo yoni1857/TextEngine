222fa2733d9f47746938e4b77d46ff2ff7f4253b
Yoni Meller <yonim1857@gmail.com>
2020-04-27 18:41:00 +0300

    Added Package Creator Tool which allows to create update packages

---
+++ PackageCreatorTool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YDK;
+using System.IO;
+
+namespace PackageCreatorTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Package package = new Package();
+            Console.Write("Author: ");
+            package.Author = Console.ReadLine();
+            Console.Write("Extract Path: ");
+            package.ExtractPath = Console.ReadLine();
+            Console.Write("Package Name: ");
+            package.Name = Console.ReadLine();
+            Console.Write("Package Version: ");
+            package.Version = Console.ReadLine();
+            Console.Write("Package Files Folder: ");
+            package.GeneratePackage(Console.ReadLine(), "./");
+        }
+    }
+}
